c707f3b3b14209b20019a55803a20e8de9688772
jaganath10486 <jaganathsegi104@gmail.com>
2022-02-02 16:49:21 +0530

    Comments are updated

--- QuizModel1/sample_Quiz.js
+++ QuizModel1/sample_Quiz.js
@@ -1,6 +1,7 @@
-const Question_container = document.getElementById('Quiz_body');
-const Submit = document.getElementById("submit");
-const Result_container = document.getElementById("result");
+const Question_container = document.getElementById('Quiz_body');//For displaying questions and choices
+const Submit = document.getElementById("submit");// Submit Button
+const Result_container = document.getElementById("result");//To display Result
+//questons and choice array
 const MyQuestions = [
     {
         question : "HTML Languages is easy to learn?",
@@ -31,41 +32,42 @@ const MyQuestions = [
     }
 ];
 
+//result function
 function result()
 {
-    var numcorrect = 0;
-    const answers = Question_container.querySelectorAll('.ans');
-    const answerContainers = Question_container.querySelectorAll('.answers');
+    var numcorrect = 0;//variable to store the correct answer
+    const answers = Question_container.querySelectorAll('.ans');//Feedback Message for Every question if correct or Wrong
+    const answerContainers = Question_container.querySelectorAll('.answers');//Choices of Every Questions
     MyQuestions.forEach((currentQUESTION, questionNo) => {
-        const answerContainer = answerContainers[questionNo];
-        const selector = `input[name=question${questionNo}]:checked`;
-        const useranswer = answerContainer.querySelector(selector);
-        if((useranswer || {}).value === currentQUESTION.correct_answer)
+        const answerContainer = answerContainers[questionNo]; //For current Question
+        const selector = `input[name=question${questionNo}]:checked`; 
+        const useranswer = answerContainer.querySelector(selector); // user answer
+        if((useranswer || {}).value === currentQUESTION.correct_answer)//here i used null {} value if user leaves the question to avoid error 
         {
             numcorrect++;
-            answerContainers[questionNo].style.color = 'green';
-            //answerContainer[].style.color = 'green';
-            answers[questionNo].style.color = 'green';
-            answers[questionNo].innerHTML = "Correct";
+            answerContainers[questionNo].style.color = 'green';//color 
+            answers[questionNo].style.color = 'green'; // message color
+            answers[questionNo].innerHTML = "Correct"; // message 
             
         }
         else
         {
-            answerContainers[questionNo].style.color = 'red';
-            answers[questionNo].style.color = 'red';
-            answers[questionNo].innerHTML = "Wrong";
+            answerContainers[questionNo].style.color = 'red';//color
+            answers[questionNo].style.color = 'red';//message color
+            answers[questionNo].innerHTML = "Wrong";//message
         }
     });
-    Result_container.innerHTML = `You Scored ${numcorrect} out of ${MyQuestions.length}`;
+    Result_container.innerHTML = `You Scored ${numcorrect} out of ${MyQuestions.length}`; // displaying crct answers
 }
 
 function displayQuiz()
 {
-    const output = [];
+    const output = [];//output
     MyQuestions.forEach((currentQUESTION, questionNo) => {
         const answer = [];
         for(letter in currentQUESTION.answer)
         {
+            //to push available choices
             answer.push(
                 `<label>
                   <input type = "radio" name = "question${questionNo}" value = "${letter}">
@@ -73,6 +75,7 @@ function displayQuiz()
                   </label>`
             );
         }
+        // to push choice container and question and div element 
         output.push(
             `<div class="Questions">${questionNo + 1})${currentQUESTION.question}</div>
               <div class ="answers">${answer.join('')}</div>
@@ -81,9 +84,9 @@ function displayQuiz()
 
     });
 
-    Question_container.innerHTML = output.join('');
+    Question_container.innerHTML = output.join('');//displaying in html
 }
 
 displayQuiz();
 
-Submit.addEventListener('click',result);
+Submit.addEventListener('click',result);//when user submit then result function called
--- README.md
+++ README.md
@@ -2,9 +2,53 @@
 # In this Repository we will show various JS Quiz Model one can develop and can Boost JS Skills 
 # Let See Models One by One
 
-1. Simple Model
-- HTML
-> In HTML we Create 3 sections to store questions and Submit Button and Feedback for Correct and Wrond answer
-> We just create 3 div elements and provide a id's to the Sections 
+# Model 1
+## HTML
+
+- In HTML we Create 3 divs and specify with 3 id's to store questions and Submit Button and Feedback for Correct and     Wrond answer
+- and we link this to js file to display questions and results
+
+## CSS
+
+- In this section we style the questions and answers and red color for wrong answer and green color for correct answer
 
 
+## JS
+
+- Get elements of html by using 'getElementById'
+- Create a Array of objects for stroing question and choice and Correct Choice, and array output for displaying questions and answer
+- Build Quiz function
+```
+We loop through every object/index of a array using forEach Loop and perofrm following Iteration
+
+ create choice array and intiallize null 
+ and loop through the current object and push the choices of object(in html format) into the choice array
+ and now push both current object question and choice array(using join function) syntax : choice.join(' ') and one more div with class = 'answer'(to display message 'correct or wrong' for every question individually) 
+ and now display every question and choices by using innerHTML = output.join(' ')
+```
+- Call the Build Quiz Function
+
+- Display Result Function
+```
+
+correct_answers (variable to store the correct answer and display after submitting) intiallize with 0
+create answer = Question_container.querySelectorAll('.ans')//to give feedback for answer
+create  answerContainers = Question_container.querySelectorAll('.answers') //  to gather all choice for each question and to check with user answers
+ 
+Now loop through every question:
+  and find the user choice by using the checked attribute
+  and now compare the user answer and answerContainer[question No]
+  if equal then we color the choice with green and display the message 'Correct' and increment 'correct_answers by 1'
+  else color the choices with red and display message 'wrong'
+  we display the message with the 'answer'.innerHTML  
+  and color the choice with style.color
+
+Finally we display the correct_answers in HTML by using the HTML id(last id) 
+```
+
+- Finally We add a eventListener to submitButton called 'Click' and we call the Display Result Function When user Submit Quiz
+- In coding i updated the comments for instructions check it!!! 
+
+# Model 2
+
+> Update Soon
